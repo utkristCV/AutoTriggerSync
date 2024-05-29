107b6cb5ba71dc861abb28d80c418befef4dc5c0
utkrist.shrestha <utkrist.shrestha@creativevirtual.com>
2024-05-29 10:08:53 +1000

    Chrome Kill task

--- auto_trigger_sync.py
+++ auto_trigger_sync.py
@@ -13,6 +13,7 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+import subprocess
 
 
 def log(text):
@@ -260,7 +261,7 @@ webhook_url = config.get('Slack', 'webhook')
 chrome_options = Options()
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("enable-automation")
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-browser-side-navigation")
@@ -281,4 +282,9 @@ project_details = get_project_details()
 trigger_and_check(all_projects)
 
 # close the browser
+driver.close()
 driver.quit()
+
+# kill all chrome tasks
+command = "taskkill /f /im chrome.exe /T"
+subprocess.run(["runas", "/user:Administrator", "cmd", "/c", command], shell=True, check=True)
