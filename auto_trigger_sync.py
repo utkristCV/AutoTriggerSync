e79e3ea8c95fcd047c50a13805a4048c098a0538
utkrist.shrestha <utkrist.shrestha@creativevirtual.com>
2024-06-28 09:56:41 +1000

    Kill Chrome task

--- auto_trigger_sync.py
+++ auto_trigger_sync.py
@@ -260,7 +260,7 @@ webhook_url = config.get('Slack', 'webhook')
 chrome_options = Options()
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("enable-automation")
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-browser-side-navigation")
@@ -281,4 +281,9 @@ project_details = get_project_details()
 trigger_and_check(all_projects)
 
 # close the browser
+driver.close()
 driver.quit()
+
+# kill all chrome tasks
+os.system("taskkill /f /im chrome.exe /T")
+
